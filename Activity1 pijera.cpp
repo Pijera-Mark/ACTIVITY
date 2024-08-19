f1d20a5f9ae6a85679114f764af7ace2540b3ce0
Pijera-Mark <trimuru704@gmail.com>
2024-08-19 14:48:25 +0800

    Add files via upload
    First

---
+++ Activity1 pijera.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int ans;
+	cout << "     Main Menu     " << "\n";
+	cout << "-------------------" << "\n\n";
+	cout << "1. To Display List Of Files" << "\n";
+	cout << "2. To Create New Directory" << "\n";
+	cout << "3. Top Change the Working Directory" << "\n";
+	cout << "4. Exit" << "\n";
+	cout << "Enter: ";
+	cin >> ans;
+	
+	
+	
+	
+	return 0;
+};
